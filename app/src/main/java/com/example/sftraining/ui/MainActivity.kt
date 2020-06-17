b53c66f7298f0d805b48ad26831d11c1229e1fbc
Ваня <ivanbeshka2002@gmail.com>
2020-06-17 19:40:09 +0500

    Create add image to prev title

--- app/src/main/java/com/example/sftraining/ui/MainActivity.kt
+++ app/src/main/java/com/example/sftraining/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sftraining.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
--- app/src/main/java/com/example/sftraining/ui/create_exer/CreateExerFragment.kt
+++ app/src/main/java/com/example/sftraining/ui/create_exer/CreateExerFragment.kt
@@ -1,16 +1,22 @@
 package com.example.sftraining.ui.create_exer
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.sftraining.R
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlin.math.abs
 
 class CreateExerFragment : Fragment() {
@@ -19,6 +25,10 @@ class CreateExerFragment : Fragment() {
     private lateinit var collapsingToolbarLayout: CollapsingToolbarLayout
     private lateinit var appBarLayout: AppBarLayout
     private lateinit var toolbar: MaterialToolbar
+    private lateinit var addTitleImage: FloatingActionButton
+    private lateinit var titleImage: AppCompatImageView
+
+    private val RC_GALLERY = 752
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +41,10 @@ class CreateExerFragment : Fragment() {
 
         btnCreateExer.setOnClickListener {
             findNavController(this).navigate(R.id.navListOfExers)
+        }
+
+        addTitleImage.setOnClickListener {
+            pickPhotoFromGallery()
         }
 
         appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
@@ -46,10 +60,31 @@ class CreateExerFragment : Fragment() {
         return root
     }
 
+    private fun pickPhotoFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        val mimeType = arrayOf("image/jpeg", "image/png")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeType)
+        startActivityForResult(intent, RC_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.e("ce", "called")
+        when (requestCode) {
+            RC_GALLERY -> {
+                val image: Uri? = data?.data
+                Glide.with(titleImage).load(image).into(titleImage)
+            }
+        }
+    }
+
     private fun initView(root: View) {
         btnCreateExer = root.findViewById(R.id.btn_create_exer)
         collapsingToolbarLayout = root.findViewById(R.id.ce_collapsing_layout)
         appBarLayout = root.findViewById(R.id.ce_app_bar_layout)
         toolbar = root.findViewById(R.id.ce_toolbar)
+        addTitleImage = root.findViewById(R.id.ce_fab_add_photo)
+        titleImage = root.findViewById(R.id.ce_title_photo)
     }
 }
