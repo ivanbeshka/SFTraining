7914b8ef32b39e7df01c506c9c04bb3ffe80556e
Gleb <gleb.demin.04@mail.ru>
2020-07-13 17:45:45 +0500

    demo settings page (stable)

--- app/src/main/java/com/example/sftraining/ui/app_settings/AppSettingsFragment.kt
+++ app/src/main/java/com/example/sftraining/ui/app_settings/AppSettingsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.example.sftraining.R
 import com.example.sftraining.ui.base.BaseFragment
 import com.example.sftraining.ui.registration.EnterActivity
@@ -15,29 +16,32 @@ import com.google.android.material.textview.MaterialTextView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class AppSettingsFragment : BaseFragment() {
+class AppSettingsFragment : PreferenceFragmentCompat() {
 
     private lateinit var btnExitAcc: MaterialButton
     private lateinit var tilPersonalInfo: TextInputLayout
     private lateinit var etPersonalInfo: TextInputEditText
     private lateinit var tvPersonalInfo: MaterialTextView
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.app_settings_fragment, container, false)
-
-        initView(root)
-
-        return root
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.app_settings_fragment, rootKey);
     }
 
-    override fun initView(root: View) {
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        val root = inflater.inflate(R.layout.app_settings_fragment, container, false)
+//
+//        initView(root)
+//
+//        return root
+//    }
+
+//    override fun initView(root: View) {
 //        btnExitAcc = root.findViewById(R.id.btn_acc_exit)
 //        tilPersonalInfo = root.findViewById(R.id.til_personal_info)
 //        etPersonalInfo = root.findViewById(R.id.et_person_info)
 //        tvPersonalInfo = root.findViewById(R.id.tv_personal_info)
-    }
+//    }
 }
