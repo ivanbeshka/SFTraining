f900fb8987c5451ed5aae01fa74a26095da17d7e
Gleb <gleb.demin.04@mail.ru>
2020-07-26 01:07:49 +0500

    add pretty showing description and exer titles in training card (in expandable layout)

--- app/src/main/java/com/example/sftraining/globalAdapters/TrainingsRecyclerAdapter.kt
+++ app/src/main/java/com/example/sftraining/globalAdapters/TrainingsRecyclerAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.sftraining.globalAdapters
 
+import android.content.Context
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,7 @@ import com.example.sftraining.R
 import com.example.sftraining.databinding.TrainingItemBinding
 import com.example.sftraining.model.Training
 import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.textview.MaterialTextView
 import net.cachapa.expandablelayout.ExpandableLayout
 import net.cachapa.expandablelayout.ExpandableLayout.OnExpansionUpdateListener
 
@@ -24,12 +27,16 @@ class TrainingsRecyclerAdapter(private var trainingsList: List<Training>) :
         OnExpansionUpdateListener {
 
         private lateinit var expandableLayout: ExpandableLayout
+        private lateinit var exerTitlesLayout: LinearLayout
         private lateinit var expandButton: ImageView
+        private lateinit var inflater: LayoutInflater
 
         fun bind(training: Training) {
             trainingItemBinding.training = training
-            initView(trainingItemBinding.root)
-            trainingItemBinding.root.findViewById<ShapeableImageView>(R.id.training_shapeable_image)
+
+            val root = trainingItemBinding.root
+
+            root.findViewById<ShapeableImageView>(R.id.training_shapeable_image)
                 .apply {
                     val cornerSize: Float = resources.getDimension(R.dimen.cornerRadius)
                     this.shapeAppearanceModel =
@@ -37,28 +44,32 @@ class TrainingsRecyclerAdapter(private var trainingsList: List<Training>) :
                     Glide.with(this.context).load(training.titleImageUri)
                         .placeholder(R.drawable.image2).centerCrop().into(this)
                 }
-            expandableLayout = trainingItemBinding.root.findViewById(R.id.expandable_layout)
-            expandButton = trainingItemBinding.root.findViewById(R.id.expand_button)
+            expandableLayout = root.findViewById(R.id.expandable_layout)
+            exerTitlesLayout = root.findViewById(R.id.exer_titles_layout)
+            expandButton = root.findViewById(R.id.expand_button)
 
-            var index = 1
-
+            val list = root.findViewById(R.id.exer_titles_layout) as LinearLayout
+            inflater = root.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             for (exer in training.exers) {
-                expandableLayout.addView(createTitleView(exer.title), index)
-                ++index
+                val vi: TextView = inflater.inflate(android.R.layout.simple_list_item_1, null) as TextView
+                vi.text = exer.title
+                list.addView(vi)
             }
+
+            Log.e("Child count", exerTitlesLayout.childCount.toString())
 
             expandableLayout.setOnExpansionUpdateListener(this)
             expandButton.setOnClickListener(this)
             trainingItemBinding.executePendingBindings()
         }
 
-        private fun createTitleView(text: String): TextView {
-            val tv = TextView(trainingItemBinding.root.context)
+        private fun createTitleView(text: String): MaterialTextView {
+            val descriptionText = trainingItemBinding.root.findViewById<MaterialTextView>(R.id.training_description)
+            val tv = MaterialTextView(trainingItemBinding.root.context)
             tv.text = text
+            tv.layoutParams = descriptionText.layoutParams
+            tv.setTextColor(Color.BLACK)
             return tv
-        }
-
-        fun initView(root: View) {
         }
 
         override fun onExpansionUpdate(expansionFraction: Float, state: Int) {
